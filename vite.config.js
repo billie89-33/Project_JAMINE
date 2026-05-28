d2aea37c8a2f36b28cb903efd0b21ea13a15353f
billie89-33 <bowvorn@gmail.com>
2026-05-29 00:12:09 +0700

    fix: cleanup vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@theme/tailwindcss' // หรือปลั๊กอินที่คุณใช้อยู่
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
   resolve: {
     alias: {
